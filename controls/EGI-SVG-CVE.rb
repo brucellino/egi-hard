315a9b9807ea03efde7dc055a88e9c7afeeae39b
Bruce Becker <brucellino@gmail.com>
2018-05-21 13:15:21 +0200

    Initial commit for travis

--- controls/EGI-SVG-CVE.rb
+++ controls/EGI-SVG-CVE.rb
@@ -7,8 +7,11 @@ title 'EGI-SVG-CVE-2018-8897'
 control 'EGI-SVG-CVE-2018-8897' do
   impact 0.7
   title 'CVE IDs CVE-2018-8897 CVE-2018-1087 CVE-2017-16939'
-  desc 'Linux Kernel vulnerabilities. kernel exception handling can allow an unprivileged user to crash the system and cause a Denial of Service (DoS) (CVE-2018-8897). \n
-            A  vulnerability concerning the Linux kernels KVM hypervisor exception handling can allow an unprivileged KVM guest user to crash the guest or, potentially, escalate their privileges in the guest (CVE-2018-1087). \n
+  desc 'Linux Kernel vulnerabilities. kernel exception handling can allow an\n
+            unprivileged user to crash the system and cause a Denial of Service (DoS)\n
+            (CVE-2018-8897). \n
+            A  vulnerability concerning the Linux kernels KVM hypervisor exception\n
+            handling can allow an unprivileged KVM guest user to crash the guest or, potentially, escalate their privileges in the guest (CVE-2018-1087). \n
             The use-after-free vulnerability flaw in XFRM mentioned in a previous alert [EGI-SVG-CVE-2017-16939] can, in some circumstances, lead to privilege escalation. '
   
   describe command('uname -r') do
